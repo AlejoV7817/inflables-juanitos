dc487bd3ee0d9becda9aaa027068f7b760a4b38b
Alejandro Vidal <alejovidal237@gmail.com>
2026-04-24 15:57:41 -0600

    Delete src directory

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './style.css' // 👈 IMPORTANTE (agrega esto)
-
-createApp(App)
-  .use(router)
-  .mount('#app')
